c35687602c9858683b2212493436a78b52271e84
Fuad Talukder <webfuadtk@gmail.com>
2024-10-03 17:45:20 +0600

    addded design

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { useGSAP } from "@gsap/react";
-import gsap from "gsap";
+// import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
 
 const App = () => {
-  useGSAP(() => {
-    gsap.to(".box", {
-      x: 1000,
-      duration: 1,
-      delay: 1,
-    });
-  });
+  // useGSAP(() => {
+  //   gsap.to(".box", {
+  //     x: 1500,
+  //     duration: 1,
+  //     delay: 1,
+  //     rotate: 720,
+  //   });
+  // });
   return (
     <main>
-      <div className="box"></div>
+      <div className="container">
+        <div className="circle"></div>
+        <div className="box"></div>
+      </div>
+      <div className="kush">
+        <div className="circle"></div>
+        <div className="box"></div>
+      </div>
     </main>
   );
 };
