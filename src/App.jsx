13a39c36a1cc5068864b3951dd42164547e3f9a1
Fuad Talukder <webfuadtk@gmail.com>
2024-11-03 18:12:27 +0600

    24 minute

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-// import { useGSAP } from "@gsap/react";
-// import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
 
 const App = () => {
-  // useGSAP(() => {
-  //   gsap.to(".box", {
-  //     x: 1500,
-  //     duration: 1,
-  //     delay: 1,
-  //     rotate: 720,
-  //   });
-  // });
+  const container = useRef();
+  useGSAP(
+    () => {
+      gsap.from(".box", {
+        rotate: 360,
+        scale: 0,
+        duration: 1,
+        opacity: 0,
+        delay: 0.5,
+      });
+    },
+    { scope: container }
+  );
   return (
     <main>
-      <div className="container">
+      <div ref={container} className="container">
         <div className="circle"></div>
         <div className="box"></div>
       </div>
